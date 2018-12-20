e678c528fd9bae4883e6c38d832aa84477a73d6d
root <root@home>
2018-12-20 14:03:11 +0300

    Start project

---
+++ app/src/main/java/com/example/alekseynd/recyclerviewimpl/RecyclerFragment.java
@@ -0,0 +1,11 @@
+package com.example.alekseynd.recyclerviewimpl;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class RecyclerFragment extends Fragment {
+
+    public static RecyclerFragment newInstance() {
+        return new RecyclerFragment();
+    }
+}
