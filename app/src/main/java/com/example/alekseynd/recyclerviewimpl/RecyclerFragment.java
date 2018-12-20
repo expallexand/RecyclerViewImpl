e53bab73a1c53429295cbec3580097d788c6cea1
root <root@home>
2018-12-20 14:22:39 +0300

    Added RecyclerView and Fragment

--- app/src/main/java/com/example/alekseynd/recyclerviewimpl/RecyclerFragment.java
+++ app/src/main/java/com/example/alekseynd/recyclerviewimpl/RecyclerFragment.java
@@ -1,11 +1,31 @@
 package com.example.alekseynd.recyclerviewimpl;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class RecyclerFragment extends Fragment {
 
+    private RecyclerView mMRecyclerView;
+
     public static RecyclerFragment newInstance() {
         return new RecyclerFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fr_recycler, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mMRecyclerView = view.findViewById(R.id.recycler);
     }
 }
